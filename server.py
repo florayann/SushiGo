0c1ea740ef8cc8b62f028a0462a2fb6ecf4fd974
Flora <xyan19>
2016-07-20 20:16:44 -0500

    add deck of 108 cards

--- server.py
+++ server.py
@@ -2,8 +2,16 @@ import sys
 import random
 import argparse
 
+
+class Deck(object):
+    def _init_(self):
+        self.cards = ["tempura"] * 14 + ["sashimi"] * 14 + ["dumpling"] * 14 + ["maki2"] * 12 + ["maki3"] * 8 + ["maki1"] * 6 + ["salmon nigiri"] * 10 + ["squid nigiri"] * 5 + ["egg nigiri"] * 5 + ["pudding"] * 10 + ["wasabi"] * 6 + ["chopsticks"] * 4]
+
+
 def main():
     pass
 
 if __name__ == "__main__":
     main()
+
+    
